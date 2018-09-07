f3181ee1c27b0d1699a0e29c689a18172cbbbec6
Nick Fragale <fragale@digilabs.io>
2018-09-07 19:34:57 +0000

    Cleanup and switch to standardized class structure

--- scripts/swiftnav_piksi_tcp.py
+++ scripts/swiftnav_piksi_tcp.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python
 
-from sbp.client.drivers.network_drivers import TCPDriver
-from sbp.client import Handler, Framer
-from sbp.settings import SBP_MSG_SETTINGS_READ_RESP, MsgSettingsWrite, MsgSettingsReadReq
-from sbp.imu import SBP_MSG_IMU_RAW
-from sbp.mag import SBP_MSG_MAG_RAW
-from sbp.navigation import SBP_MSG_BASELINE_HEADING_DEP_A, SBP_MSG_POS_LLH, SBP_MSG_BASELINE_NED
-from datetime import datetime
-from nav_msgs.msg import Odometry
-from sensor_msgs.msg import Imu, MagneticField, NavSatFix
-from std_msgs.msg import String, Int32, Bool
-from geometry_msgs.msg import TwistStamped
 import subprocess
 import os
 import struct
@@ -19,115 +8,81 @@ import sys
 import rospy
 import numpy as np
 import math
+from sbp.client.drivers.network_drivers import TCPDriver
+from sbp.client import Handler, Framer
+from sbp.settings import SBP_MSG_SETTINGS_READ_RESP, MsgSettingsWrite, MsgSettingsReadReq
+from sbp.imu import SBP_MSG_IMU_RAW
+from sbp.navigation import SBP_MSG_BASELINE_HEADING_DEP_A, SBP_MSG_POS_LLH, SBP_MSG_BASELINE_NED
+from datetime import datetime
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu, MagneticField, NavSatFix
+from std_msgs.msg import String, Int32, Bool
+from geometry_msgs.msg import TwistStamped
 
-# Initialize message and publisher structures
-DRIVE_DIRECTION = "forward"
-DEFAULT_COMM_STATUS = True
-NCAT_PROC = None
-RTK_ENABLE = True 
-RTK_DISABLE = False
-old_x = 0
-old_y = 0
 
-# TOPIC: swift_gps/imu/raw
-# This topic publishes the current raw IMU output of the swift nav gps
-imu_msg = Imu()
-imu_pub = rospy.Publisher('swift_gps/imu/raw', Imu, queue_size=10)
+class SwiftNavDriver(object):
 
-# TOPIC: swift_gps/imu/mag
-# This topic puslished the current magnotometer reading of the swift nav gps
-mag_msg = MagneticField()
-mag_pub = rospy.Publisher('swift_gps/imu/mag', MagneticField, queue_size=10)
+    def __init__(self):
+        rospy.loginfo("[RR_SWIFTNAV_PIKSI] Initializing")
 
-# TOPIC: swift_gps/llh/position
-# This position solution message reports the absolute geodetic coordinates and the status (single point
-# vs pseudo-absolute RTK) of the position solution. If the rover receiver knows the surveyed position of
-# the base station and has an RTK solution, this reports a pseudo-absolute position solution using the
-# base station position and the rovers RTK baseline vector. The full GPS time is given by the preceding
-# MSG GPS TIME with the matching time-of-week (tow).
+        # Initialize message and publisher structures
+        self.drive_direction = "forward"
+        self.ncat_process = None
+        self.old_x = 0
+        self.old_y = 0
 
-# TOPIC: swift_gps/llh/n_sats
-# This topic reports the number of satelites used to obtain llh position 
+        # TOPIC: swift_gps/llh/fix_mode
+        # This topic reports the fix_mode of llh position
+        # 0 - Invalid 
+	# 1 - Single Point Position (SSP)
+	# 2 - Differential GNSS (DGNSS)
+	# 3 - Float RTK
+	# 4 - Fixed RTK
+	# 5 - Dead Reckoning
+        # 6 - Satellite-based Augmentation System (SBAS)
 
-# TOPIC: swift_gps/llh/fix_mode
-# This topic reports the fix_mode of llh position
-# 0 - Invalid 
-# 1 - Single Point Position (SSP)
-# 2 - Differential GNSS (DGNSS)
-# 3 - Float RTK
-# 4 - Fixed RTK
-# 5 - Dead Reckoning
+	# ROS Publishers
+	self.pub_imu = rospy.Publisher('/swift_gps/imu/raw', Imu, queue_size=10)
+	self.pub_llh = rospy.Publisher('/swift_gps/llh/position', NavSatFix, queue_size=3)
+	self.pub_llh_n_sats = rospy.Publisher('/swift_gps/llh/n_sats', Int32, queue_size=3)
+	self.pub_llh_fix_mode = rospy.Publisher('/swift_gps/llh/fix_mode', Int32, queue_size=3)
+	self.pub_ecef_odom = rospy.Publisher('/swift_gps/baseline/ecef/position', Odometry, queue_size=3)
 
-llh_msg = NavSatFix()
-llh_pub = rospy.Publisher('swift_gps/llh/position', NavSatFix, queue_size=3)
-
-llh_n_sats_msg = Int32()
-llh_n_sats_pub = rospy.Publisher('swift_gps/llh/n_sats', Int32, queue_size=3)
-
-llh_fix_mode_msg = Int32()
-llh_fix_mode_pub = rospy.Publisher('swift_gps/llh/fix_mode', Int32, queue_size=3)
-
-# TOPIC: swift_nav/baseline/odom
-# This topic provides an X Y Z position in ECEF (earth centered earth fixed) coordinates of the swift_nav gps with respect to the basestation. This is the 
-# topic that should be used for navigation purposes and can be accurate to cm level when RTK fix is achieved
-
-# TOPIC: swif_gps/baseline/n_sats
-# This topic provides the number of sattelites currently being used for the RTK fix algorythm
-
-# TOPIC: swift_nav/baseline/fix_mode
-# This topic  publishes the current fix mode of baseline/ecef/position topic when RTK float or fix is achieved
-# 0 - Invalid
-# 1 - Reserved
-# 2 - Differential GNSS (DGNSS)
-# 3 - Float RTK
-# 4 - Fixed RTk
-
-ecef_odom_msg = Odometry()
-ecef_odom_pub = rospy.Publisher('swift_gps/baseline/ecef/position', Odometry, queue_size=3)
-
-ecef_n_sats_msg = Int32()
-ecef_n_sats_pub = rospy.Publisher('swift_gps/baseline/ecef/n_sats', Int32, queue_size=3, latch=True)
-
-ecef_fix_mode_msg = Int32()
-ecef_fix_mode_pub = rospy.Publisher('swift_gps/baseline/ecef/fix_mode', Int32, queue_size=3, latch=True)
+	# ROS Subscriber
+	self.sub_rtk_cmd = rospy.Subscriber("/swift_gps/enable_comms", Bool, self.enable_comms_cb)
+        self.sub_cmd_vel = rospy.Subscriber("/cmd_vel/managed", TwistStamped, self.cmd_vel_cb)            
 
 
 
-# swift_gps/comms_disabled provides feedback as to whether comms has been disabled or not  
-comms_disabled_msg = Bool()
-comms_disabled_pub = rospy.Publisher('swift_gps/comms_disabled', Bool, queue_size=3, latch=True)
+    def cmd_vel_cb(self, cmd_vel):
+        if cmd_vel.twist.linear.x > 0:
+            self.drive_direction = "forward"
+        if cmd_vel.twist.linear.x < 0:
+            self.drive_direction = "reverse"
 
-# Variable to track is ncat process is running
-# note: ncat if the linux networking tool used to tunnel RTK data through the main PC
-NCAT_PROC=None
-
-# Initialize ROS node 
-rospy.init_node('swift_gps_node')
-
-def disable_comms_cb(msg):
-    global RTK_ENABLE, RTK_DISABLE, NCAT_PROC
-    # determine what message was - do we need high accuracy or not?
-    if (msg.data == False and NCAT_PROC is None):
-        RTK_ENABLE = True
-    elif (msg.data == True and NCAT_PROC is not None):
-        RTK_DISABLE = True
-
-# swift_gps/disable_comms provides a way of stopping the SwiftNav GPS from communicating with the base station during times when RTK fix is not needed
-# in order to save network bandwidth
-rtk_cmd_sub = rospy.Subscriber("swift_gps/disable_comms", Bool, disable_comms_cb)
+    def enable_comms_cb(self, msg):
+        if (msg.data == True):
+	    # Note: ncat is the linux networking tool used to tunnel the RTK data through the main PC
+	    if (self.ncat_process is None):
+	        self.ncat_process = subprocess.Popen('/usr/bin/ncat -l 1.2.3.55 55555 --sh-exec "/usr/bin/ncat 65.132.94.146 55555"', shell=True)
+                rospy.loginfo("[RR_SWIFNAV_PIKSI] GPS comms enabled, ncat started")
+	    else:
+	        rospy.logwarn("[RR_SWIFTNAV_PIKSI] GPS comms already enabled, ignoring request")
+        if (msg.data == False):
+	    if (self.ncat_process is not None):
+	        subprocess.call(["kill", "-9", "%d" % self.ncat_process.pid])
+	        CAT_PROC.wait()
+	        os.system('killall ncat')
+                rospy.loginfo("[RR_SWIFT_NAV_PIKSI] GPS comms disables, ncat stopped")
+	        self.ncat_process=None
+	    else:
+	        rospy.logwarn("[RR_SWIFTNAV_PIKSI] RTK GPS already disabled, ignoring request")
 
 def publish_baseline_msg(msg, **metadata):
-    global comms_disabled_msg, old_x, old_y
-    global DRIVE_DIRECTION
+    if self.comms_disabled:
+        return
 
-    # Publish n_sats and fix_mode
-    ecef_n_sats_msg.data = msg.n_sats
-    ecef_fix_mode_msg.data = msg.flags
-    ecef_n_sats_pub.publish(ecef_n_sats_msg)
-    ecef_fix_mode_pub.publish(ecef_fix_mode_msg)
-    
-
-    # Obtain position and covariance and convert from mm to m
+    # Obtain position and accuracies and convert from mm to m
     x_pos = float(msg.e)/1000
     y_pos = float(msg.n)/1000
     z_pos = float(msg.d)/1000
@@ -135,32 +90,28 @@ def publish_baseline_msg(msg, **metadata):
     v_accuracy = float(msg.v_accuracy)/1000
 
 
-    if comms_disabled_msg.data:
-        rospy.logwarn_throttle(60, "SwiftNav GPS comms are disabled")
+    if (x_pos,y_pos) == (0.0,0.0):
+        rospy.logwarn_throttle(10,"SwiftNav GPS baseline reported x=0 y=0. Message not published")
         return
-    else:
-        if x_pos == 0.0 and y_pos == 0.0:
-            rospy.logwarn_throttle(10,"SwiftNav GPS baseline reported x=0 y=0. Message not published")
-            return
-   
+
     # Build the ROS Odometry message
+    ecef_odom_msg = Odometry()
     ecef_odom_msg.child_frame_id = 'gps_link'
     ecef_odom_msg.header.stamp = rospy.Time.now()
     ecef_odom_msg.header.frame_id = 'map'
     ecef_odom_msg.pose.pose.position.x = x_pos
     ecef_odom_msg.pose.pose.position.y = y_pos
     ecef_odom_msg.pose.pose.position.z = 0
-    
-    # Calculate distance travelled since last RTK measurement
-    if DRIVE_DIRECTION=="forward":
-        delta_x = x_pos - old_x
-        delta_y = y_pos - old_y
-    if DRIVE_DIRECTION=="reverse":
-        delta_x = old_x - x_pos
-        delta_y = old_y - y_pos
 
+    # Calculate distance travelled since last RTK measurement
+    if self.drive_direction=="forward":
+        delta_x = x_pos - self.previous_x
+        delta_y = y_pos - self.previous_y
+    if self.drive_direction=="reverse":
+        delta_x = self.previous_x - x_pos
+        delta_y = self.previous_y - y_pos
     distance_travelled = np.sqrt(np.power(delta_x,2) + np.power(delta_y,2))
- 
+
     # Normalize the orientation vector
     if (distance_travelled==0):
         delta_x_hat = 0
@@ -175,13 +126,12 @@ def publish_baseline_msg(msg, **metadata):
         ecef_odom_msg.pose.pose.orientation.w = np.cos(angle/2)
 
     # Update the old positions
-    old_x = x_pos
-    old_y = y_pos
-    
+    self.previous_x = x_pos
+    self.previous_y = y_pos
+        
     # Calculate the position covariances using the accuracy reported by the Piksi
-    cov_x = h_accuracy
-    cov_y = cov_x
-
+    cov_x = cov_y = h_accuracy
+        
     # Calculate the orientation covariance, the further we have moved the more accurate orientation is
     if (0<=distance_travelled and distance_travelled<=0.04):
         theta_accuracy = 1000
@@ -195,20 +145,19 @@ def publish_baseline_msg(msg, **metadata):
         theta_accuracy = -1
         rospy.logerr_throttle(5,"distance travelled was negative")
 
-
     cov_theta = theta_accuracy
-
     ecef_odom_msg.pose.covariance = [cov_x, 0, 0, 0, 0, 0,
-                                    0, cov_y, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, 0,
-                                    0, 0, 0, 0, 0, cov_theta]
-    # Publish topics
-    ecef_odom_pub.publish(ecef_odom_msg)
+                                        0, cov_y, 0, 0, 0, 0,
+                                        0, 0, 0, 0, 0, 0,
+                                        0, 0, 0, 0, 0, 0,
+                                        0, 0, 0, 0, 0, 0,
+                                        0, 0, 0, 0, 0, cov_theta]
+    # Publish earth-centered-earth-fixed message
+    pub_ecef_odom.publish(ecef_odom_msg)
 
 
 def publish_imu_msg(msg, **metadata):
+    imu_msg = Imu()
     imu_msg.header.stamp = rospy.Time.now()
     imu_msg.header.frame_id = 'gps_link'
     # acc_range scale settings to +- 8g (4096 LSB/g), gyro_range to +-1000 (32.8 LSB/deg/s)
@@ -234,15 +183,13 @@ def publish_imu_msg(msg, **metadata):
     imu_msg.linear_acceleration_covariance= [0.01,0,0,
                                              0,0.01,0,
                                              0,0,0.01]
-
     # Publish to /gps/imu/raw
-    imu_pub.publish(imu_msg)
-
+    pub_imu.publish(imu_msg)
 
 
 def publish_llh_msg(msg, **metadata):
-    # note there was no time update in the llh_msg
-    #llh_msg.header.stamp = rospy.Time.now()
+    rospy.logwarn("Publishing llh message")
+    llh_msg = NavSatFix()
     llh_msg.latitude = msg.lat
     llh_msg.longitude = msg.lon
     llh_msg.altitude = msg.height
@@ -250,42 +197,14 @@ def publish_llh_msg(msg, **metadata):
     llh_msg.position_covariance = [9,0,0,
                                    0,9,0,
                                    0,0,9]
-    llh_n_sats_msg = msg.n_sats
-    llh_fix_mode_msg = msg.flags
-
     # Publish ROS messages
-    llh_pub.publish(llh_msg)
-    llh_n_sats_pub.publish(llh_n_sats_msg)
-    llh_fix_mode_pub.publish(llh_fix_mode_msg)
+    pub_llh.publish(llh_msg)
+    pub_llh_n_sats.publish(Int32(msg.n_sats))
+    pub_llh_fix_mode.publish(Int32(msg.flags))
 
 
-
-def publish_mag_msg(msg, **metadata):
-    mag_msg.header.stamp = rospy.Time.now()
-    mag_msg.header.frame_id = 'gps_link'
-    # should be in Tesla - chip stores data in two's complement 13-bits data
-    # according to /usr/local/lib/python2.7/dist-packages/sbp/mag.py swiftnav does not do anything special to it
-    # according to Bosch's datasheet, output is scaled by 16, so divide by 16 to get to uTesla
-    # and divide further by 100 so it is in Gauss
-    mscale=0.01/16
-    mag_msg.magnetic_field.x = (msg.mag_x*mscale)
-    mag_msg.magnetic_field.y = (msg.mag_y*mscale)
-    mag_msg.magnetic_field.z = (msg.mag_z*mscale)
-    mag__msg.magnetic_field_covariance= [1,0,0,
-                                      0,1,0,
-                                      0,0,1]
-
-    # Publish to /gps/imu/mag
-    mag_pub.publish(mag_msg)
-
-
-class SettingMonitor(object):
-    """Class to monitor Settings via SBP messages
-    Parameters
-    ----------
-    None
-    """
-
+class SwiftMonitor(object):
+    ## Class to monitor Settings via SBP messages
     def __init__(self):
         self.settings = []
 
@@ -305,8 +224,7 @@ class SettingMonitor(object):
         expire = time.time() + wait_time
         ok = False
         while not ok and time.time() < expire:
-            settings = filter(lambda x: (x[0], x[1]) == (section, setting),
-                              self.settings)
+            settings = filter(lambda x: (x[0], x[1]) == (section, setting), self.settings)
             # Check to see if the last setting has the value we want
             if len(settings) > 0:
                 ok = settings[-1][2] == value
@@ -315,36 +233,26 @@ class SettingMonitor(object):
         return ok
 
     def clear(self, section=None, setting=None, value=None):
-        """Clear settings"""
-        match = map(lambda (x,y,z): all((section is None or x == section,
-                                         setting is None or y == setting,
-                                         value is None or z == value)),
-                    self.settings)
-
-        keep = filter(lambda (setting,remove): not remove,
-                      zip(self.settings,match))
-
+        match = map(lambda (x,y,z): all((section is None or x == section, setting is None or y == setting, value is None or z == value)), self.settings)
+        keep = filter(lambda (setting,remove): not remove, zip(self.settings,match))
         self.settings[:] = map(lambda x: x[0], keep)
 
 def sbp_print_setting(sbp_msg, **metadata):
     print sbp_msg
 
-def cmd_vel_cb(cmd_vel):
-    global DRIVE_DIRECTION
-    if cmd_vel.twist.linear.x > 0:
-        DRIVE_DIRECTION = "forward"
-    if cmd_vel.twist.linear.x < 0:
-        DRIVE_DIRECTION = "reverse"
+if __name__ == "__main__":
+    rospy.init_node('rr_swiftnav_gps_node')
+    swift_nav_driver = SwiftNavDriver()
 
-def main():
-    global RTK_ENABLE, RTK_DISABLE, NCAT_PROC, DEFAULT_COMM_STATUS
-    global comms_disabled_msg
 
+    # ROS Parameters
     ipaddr = rospy.get_param('default_param', '1.2.3.10')
     tcp_port = rospy.get_param('default_param', '55555')
 
-    monitor = SettingMonitor()
 
+    # Create SwiftNav Callbacks
+    monitor = SwiftMonitor()
+    rospy.loginfo("[RR_SWIFTNAV_PIKSI] 0")
     with TCPDriver(ipaddr, tcp_port) as driver:
         with Handler(Framer(driver.read, driver.write)) as source:
             driver.flush()
@@ -352,54 +260,14 @@ def main():
             # Capture setting messages
             source.add_callback(monitor.capture_setting,SBP_MSG_SETTINGS_READ_RESP)
             source.add_callback(sbp_print_setting, SBP_MSG_SETTINGS_READ_RESP)
+            rospy.loginfo("[RR_SWIFTNAV_PIKSI] 1")
             source.add_callback(publish_baseline_msg, SBP_MSG_BASELINE_NED)
             source.add_callback(publish_imu_msg,SBP_MSG_IMU_RAW)
-            #source.add_callback(publish_mag_msg,SBP_MSG_MAG_RAW)
             source.add_callback(publish_llh_msg,SBP_MSG_POS_LLH)
+            rospy.loginfo("[RR_SWIFTNAV_PIKSI] 2")
             source.start
+            rospy.loginfo("[RR_SWIFTNAV_PIKSI] 3")
+    while not rospy.is_shutdown():
+        time.sleep(0.1)
+    rospy.spin()
 
-            cmd_vel_sub = rospy.Subscriber("/cmd_vel/managed", TwistStamped, cmd_vel_cb)            
-
-            # we leave the swiftnav tcp_client0 on all the time, but pointing to 1.2.3.55:55555 and only start the ncat forwarder when it is needed
-            comms_disabled_msg.data = DEFAULT_COMM_STATUS
-            comms_disabled_pub.publish(comms_disabled_msg)
-            os.system('killall ncat > /dev/null 2>&1')
-
-            while not rospy.is_shutdown():
-                # Enable basestation communication if rtk_enable_global is TRUE 
-                # Note: ncat is the linux networking tool used to tunnel the RTK data through the main PC 
-                if (RTK_ENABLE):
-                    if (NCAT_PROC is None):
-                        NCAT_PROC = subprocess.Popen('/usr/bin/ncat -l 1.2.3.55 55555 --sh-exec "/usr/bin/ncat 65.132.94.146 55555"', shell=True)
-                        rospy.loginfo("GPS tunnel running...")
-                        # done setting, set the rtk_enable boolean to False
-                        RTK_ENABLE=False
-                        comms_disabled_msg.data = False
-                        comms_disabled_pub.publish(comms_disabled_msg)
-                # Disable basestation communication if rtf_disable_global is TRUE
-                # Note: ncat is the linux networking tool used to tunnel the RTK data through the main PC
-                if (RTK_DISABLE):
-                    if (NCAT_PROC is not None):
-                        subprocess.call(["kill", "-9", "%d" % NCAT_PROC.pid])
-                        NCAT_PROC.wait()
-                        os.system('killall ncat')
-                        rospy.loginfo("GPS tunnel killed...")
-                        NCAT_PROC=None
-                        # done setting, set the rtk_disable boolean to False
-                        RTK_DISABLE=False
-                        comms_disabled_msg.data = True
-                        comms_disabled_pub.publish(comms_disabled_msg)
-            	time.sleep(0.1)
-            sys.exit()
-
-if __name__ == "__main__":
-    try:
-        main()
-    except rospy.ROSInterruptException:
-        # kill the ncat process
-        if (NCAT_PROC is not None):
-            subprocess.call(["kill", "-9", "%d" % NCAT_PROC.pid])
-            NCAT_PROC.wait()
-        pass
-
-    #except KeyboardInterrupt:
